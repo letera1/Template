99363e5e38f336a29e1b6fdd90b5ca565f647a78
Letera Tujo <168364436+letera1@users.noreply.github.com>
2025-07-04 23:50:10 +0300

    Update services.jsx

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -7,7 +7,7 @@ export const Services = (props) => {
         <div className="section-title">
           <h2>Our Services</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
+            Lorem ipsum dolor sit amet, consectetur adipiscing roelit duis sed
             dapibus leonec.
           </p>
         </div>
