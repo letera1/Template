ba1809c3e4c24b58080da1ad3c19a330b4c32024
Letera Tujo <168364436+letera1@users.noreply.github.com>
2025-07-04 23:49:23 +0300

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -12,7 +12,7 @@ export const Header = (props) => {
                   {props.data ? props.data.title : "Loading"}
                   <span></span>
                 </h1>
-                <p>{props.data ? props.data.paragraph : "Loading"}</p>
+                <p>{props.data ? props.data.paragraph : "loading"}</p>
                 <a
                   href="#features"
                   className="btn btn-custom btn-lg page-scroll"
