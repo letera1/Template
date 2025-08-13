18a89cfb90c5ce30b2390eece13832cb2799a7ab
Letera Tujo Birri <168364436+letera1@users.noreply.github.com>
2025-08-13 19:35:53 +0300

    Update navigation.jsx

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
-            Landing Pages
+            Landing Page
           </a>{" "}
         </div>
 
@@ -29,7 +29,7 @@ export const Navigation = (props) => {
           <ul className="nav navbar-nav navbar-right">
             <li>
               <a href="#features" className="page-scroll">
-                Features
+                Feature
               </a>
             </li>
             <li>
@@ -39,7 +39,7 @@ export const Navigation = (props) => {
             </li>
             <li>
               <a href="#services" className="page-scroll">
-                Services
+                Service
               </a>
             </li>
             <li>
@@ -49,12 +49,12 @@ export const Navigation = (props) => {
             </li>
             <li>
               <a href="#testimonials" className="page-scroll">
-                Testimonials
+                Testimonial
               </a>
             </li>
             <li>
               <a href="#team" className="page-scroll">
-                Team
+                Teams
               </a>
             </li>
             <li>
