986031c782ff4971a908ad6686ff80c614704b56
Letera Tujo Birri <168364436+letera1@users.noreply.github.com>
2025-08-13 19:35:01 +0300

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -17,7 +17,7 @@ export const Header = (props) => {
                   href="#features"
                   className="btn btn-custom btn-lg page-scroll"
                 >
-                  Learn More
+                  Learn Mores
                 </a>{" "}
               </div>
             </div>
