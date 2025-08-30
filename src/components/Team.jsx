92907f61db82308d22a9c760448761b22b1760fc
Letera Tujo <168364436+letera1@users.noreply.github.com>
2025-08-30 22:25:01 +0300

    Update Team.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -7,7 +7,7 @@ export const Team = (props) => {
         <div className="col-md-8 col-md-offset-2 section-title">
           <h2>Meet the Teams</h2>
           <p>
-            Lorem ipsum dolors sita amette, consectetur adipiscing elit duis sed
+            Lorem ipsum dolors sita amete, consectetur adipiscing elit duis sedo
             dapibus leoneccu.
           </p>
         </div>
